42e6f7ae84cebc04c5ad8b30786aae6830f7cb39
christophrus <christophrus@protonmail.com>
2019-04-05 01:40:18 +0200

    add helmet.js and error handling to server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+var helmet      = require('helmet');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -14,6 +15,7 @@ var app = express();
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(helmet());
 
 
 
@@ -44,6 +46,24 @@ app.use(function(req, res, next) {
     .type('text')
     .send('Not Found');
 });
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage;
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message;
+  } else {
+    // generic or custom error
+    errCode = err.status || 500;
+    errMessage = err.message || 'Internal Server Error';
+  }
+  res.status(errCode).send(errMessage);
+})
 
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
